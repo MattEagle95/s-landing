1963a866cefd115f952063fee8b8ba766b4e1e15
skrambl <55457634+MattEagle95@users.noreply.github.com>
2023-05-06 14:49:52 +0200

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,9 @@ export default function RootLayout({
         {children}
         <Script
           async
-          src={new URL(
+          src={process.env.NEXT_PUBLIC_STATS_HOST && new URL(
             'script.js',
-            process.env.NEXT_PUBLIC_STATS_HOST || '',
+            process.env.NEXT_PUBLIC_STATS_HOST,
           ).toString()}
           data-website-id={process.env.NEXT_PUBLIC_STATS_WEBSITE_ID}
         />
